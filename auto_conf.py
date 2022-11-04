77defe6bc551060e723ebeba49eac7036d8e2ea1
0xSp3ctra <0xspectra@proton.me>
2022-11-04 15:31:53 +0100

    génération mot de passe cisco secret, début conf switchport et vlan

---
+++ auto_conf.py
@@ -0,0 +1,9 @@
+######################################
+#       !/usr/bin/env python3        #
+#       -*- coding: utf-8 -*-        #
+# Script by Colin Ledru (@0xSp3ctra) #
+######################################
+
+from cisco_pwd_hash import cisco_pwd
+
+print(cisco_pwd(5, "test"))
---
+++ cisco_pwd_hash.py
@@ -0,0 +1,33 @@
+from cisco_psswd_hasher import pwd_check, InvalidPassword, type5, type7, type8, type9
+from sys import exit
+
+def pwd_input(pwd: str):
+    while True:
+        try:
+            pwd_check(pwd)
+            return pwd
+        except InvalidPassword as exception_string:
+            print(f'{exception_string} Please try again or press CTRL + C to exit: ')
+        except KeyboardInterrupt:
+            exit()
+
+def cisco_pwd(passwd_type: int, pwd: str):
+    if passwd_type == 5:
+        cleartext_password = pwd_input(pwd)
+        passwd_hash = type5(cleartext_password)
+        return passwd_hash
+    elif passwd_type == 7:
+        cleartext_password = pwd_input(pwd)
+        passwd_hash = type7(cleartext_password)
+        return passwd_hash
+    elif passwd_type == 8:
+        cleartext_password = pwd_input(pwd)
+        passwd_hash = type8(cleartext_password)
+        return passwd_hash
+    elif passwd_type == 9:
+        cleartext_password = pwd_input(pwd)
+        passwd_hash = type9(cleartext_password)
+        return passwd_hash
+    else:
+        print('\n' + 'Invalid option. Please enter 5, 7, 8 or 9 or press CTRL + C to exit: ' + '\n')
+        exit()
