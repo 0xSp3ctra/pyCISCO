4482ca33d63828e28af5857cea1d2772217cdde8
0xSp3ctra <0xspectra@proton.me>
2022-11-05 17:07:22 +0100

    delete unused import module

--- cisco_psswd_hasher/__init__.py
+++ cisco_psswd_hasher/__init__.py
@@ -2,7 +2,6 @@ import random, base64, sys, scrypt
 from backports.pbkdf2 import pbkdf2_hmac
 from passlib.hash import md5_crypt
 from passlib.hash import cisco_type7
-from backports import *
 
 # Translate Standard Base64 table to Cisco Base64 Table used in Type8 and TYpe 9                                                
 std_b64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
