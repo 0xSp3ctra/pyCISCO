889bacb386a80d0bcc46217deb9f9c3b08c4e0b6
0xSp3ctra <0xspectra@proton.me>
2022-11-06 12:10:55 +0100

    possibility to configure switchport on interfaces

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from cisco_pwd_hash import cisco_pwd
-
+from colorama import Fore
 class ConfigSwitch():
 
     def add_hostname(hostname: str) -> str:
@@ -24,3 +24,40 @@ class ConfigSwitch():
         vlan_mask = vlan_infos.split(':')[3]
         vlan_config_line = f"interface Vlan{vlan_id}\n name {vlan_name}\n ip address {vlan_ip} {vlan_mask}"
         return vlan_config_line, vlan_id
+
+    def switchport(vlans_id: str) -> str:
+        vlans_id = vlans_id.split(',')
+        switchport_line_all = []
+
+        for vlan in vlans_id:
+            switchport_mode = str(input("Enter the switchport mode " + Fore.RED + "(access, voice, trunk) " + Fore.RESET + "" + f"for the vlan {vlan} : "))
+
+            if switchport_mode == "access":
+                switchport_line = f" switchport mode access\n switchport access vlan {vlan}"
+                switchport_line_all.append(switchport_line)
+
+            elif switchport_mode == "voice":
+                switchport_line = f" switchport mode access\n switchport voice vlan {vlan}"
+                switchport_line_all.append(switchport_line)
+
+            elif switchport_mode == "trunk":
+                switchport_line = f" switchport mode trunk\n switchport trunk allowed vlan {vlan}"
+                switchport_line_all.append(switchport_line)
+
+            else:pass
+        # for line in switchport_line_all:
+        #     print(str(line))
+
+    def configure_interface() -> str:
+        interface_type = str(input("\nDo you want to configure :\n[1] FastEthernet interfaces\n[2] GigabitEthernet interfaces\n"))
+        if interface_type == '1':
+            interface_choice = str(input("\nEnter the numeros of the interfaces you want to configure " + Fore.RED + "(1 2 3 X): " + Fore.RESET))
+            interfaces_to_configure = interface_choice.split()
+
+            for interface in interfaces_to_configure:
+                print("\nWhat do you want to add on the interface " + Fore.RED +  f"Fa0/{interface} ?" + Fore.RESET)
+                choice = int(input("[1] Add vlan(s) on interface with switchport\n"))
+
+                if choice == 1:
+                    vlans = str(input("Enter the numero of vlan to configure on this port " + Fore.RED + "(10,20,X) : " + Fore.RESET))
+                    ConfigSwitch.switchport(vlans)
