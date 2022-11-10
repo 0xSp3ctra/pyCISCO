6e3c372e6057fc0491b7b05869542c5ca5f4cadf
0xSp3ctra <0xspectra@proton.me>
2022-11-10 10:51:47 +0100

    add test for ip route

--- test_config_switch_methods.py
+++ test_config_switch_methods.py
@@ -28,5 +28,8 @@ class TestConfigSwitch(unittest.TestCase):
     def test_add_default_gateway(self):
         self.assertEqual(ConfigSwitch.add_default_gateway("10.120.11.11"), "ip default-gateway 10.120.11.11")
         
+    def test_add_ip_route(self):
+        self.assertEqual(ConfigSwitch.add_ip_route("10.10.10.10:255.0.0.0 11.11.11.11:255.0.0.0"), "ip route 10.10.10.10 255.0.0.0 11.11.11.11 255.0.0.0")
+
 if __name__ == '__main__':
     unittest.main()
